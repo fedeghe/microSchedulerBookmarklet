1adf4070da8e0958fe27ad5d7a46765b6396b175
fedeghe <fedeghe@gmail.com>
2023-03-31 22:26:33 +0200

    minor refactor

--- source/bookmarklet.js
+++ source/bookmarklet.js
@@ -1,1 +1,5 @@
-window.open('https://fedeghe.github.io/microSchedulerBookmarklet/index.html','Scheduler','width=300,height=100,status=0,toolbar=0,menubar=0,resizebar=0,popup=1,noopener=1,noreferrer=1');
+window.open(
+    'https://fedeghe.github.io/microSchedulerBookmarklet/index.html',
+    'Scheduler',
+    'width=300,height=100,status=0,toolbar=0,menubar=0,location=0,resizable=0,popup=1,noopener=1,noreferrer=1'
+);
--- source/index.js
+++ source/index.js
@@ -6,16 +6,8 @@ const countdown = require('@fedeghe/countdown');
                 fontFamily: 'verdana',
                 padding: '5px',
                 backgroundColor: 'white',
-                // position:'fixed',
-                // top:'-5px',
-                // right:'-5px',
                 paddingTop:'10px',
                 paddingRight:'20px',
-                // top:0,
-                // right:0,
-                // width:'300px',
-                // height:'50px',
-                // border:'1px dashed #aaa',
                 borderRadius:'5px',
                 opacity:'0.7',
                 display:'flex',
@@ -23,14 +15,6 @@ const countdown = require('@fedeghe/countdown');
                 flexDirection: 'column',
                 alignItems: 'center'
             },
-            // close: {
-            //     position:'absolute',
-            //     right:'10px',
-            //     top:'5px',
-            //     width:'10px',
-            //     height:'10px',
-            //     cursor:'pointer'
-            // },
             plus: {
                 width:'20px',
                 height:'20px',
@@ -60,129 +44,104 @@ const countdown = require('@fedeghe/countdown');
                 transition: 'filter .3s ease-in-out'
             }
         },
-        target = document.body;
-    
-    function sec2time(sec) {
-        var h = ~~ (sec / 3600),
-            m = ~~ ((sec % 3600)/ 60),
-            s = ~~ (sec % 60);
-        return `${h}h ${m}m ${s}s`;
-    }
-    function create(tag, {attrs = null, style = null}) {
-        var t = document.createElement(tag);
-        if(attrs) for(a in attrs) t.setAttribute(a, attrs[a]);
-        if(style) for(s in style) t.style[s] = style[s];
+        target = document.body,
+        create = function(tag, {attrs = null, style = null}) {
+            var t = document.createElement(tag);
+            if(attrs) for(a in attrs) t.setAttribute(a, attrs[a]);
+            if(style) for(s in style) t.style[s] = style[s];
+            return t;
+        },
+        container = create('div', {style: styles.container}),
+        fileInput = create('input', {attrs: {type:'file', name:'file', accept:'application/json'}}),
+        label = create('div', {style:styles.label}),
+        progress = create('progress', {style:styles.progress, attrs:{value: 0, max:100}}),
+        remaining = create('div', {style:styles.remaining});
+        end = create('div', {style:styles.complete});
+        newrun = create('div', {style:styles.newrun});
+        rerun = create('div', {style:styles.rerun}),
+        memRun = function (){},
 
-        return t;
-    }
-    function schedule(config, setters, show, complete){
-        // console.log(JSON.stringify(config, null, 2))
-        runSchedule(Object.entries(config), 0, setters, complete)
-        show()
-    }
-    
-    function runSchedule(schedules, index, setters, complete){
-        var label = schedules[index][0],
-            time = schedules[index][1];
-        
-        setters.label(label)
-        setters.progress(1000)
-        setters.remaining(sec2time(time*60))
-        countdown(() => {
-            console.log(schedules.length,  index+1,  schedules.length > index+1)
-            if (schedules.length > index+1) {
-                runSchedule(schedules, index+1, setters, complete)
-            } else {
-                console.log('complete')
-                complete()
-            }
-        }, time*60e3)
-        .onTick(({progress, remaining}) => {
-            // console.log({progress, remaining})
-            setters.progress(100-progress)
-            setters.remaining(sec2time(Math.ceil(remaining/1000)))
-        },1e3).run()
-    }
+        sec2time = function(sec) {
+            var h = ~~ (sec / 3600),
+                m = ~~ ((sec % 3600)/ 60),
+                s = ~~ (sec % 60);
+            return `${h}h ${m}m ${s}s`;
+        },
+        remove = function(p, cs){cs.forEach(function(c){p.removeChild(c)})},
+        append = function(p, cs){cs.forEach(function(c){p.appendChild(c)})},
+        schedule = function(config, setters, show, complete){
+            runSchedule(Object.entries(config), 0, setters, complete);
+            show();
+        },
+        runSchedule = function(schedules, index, setters, complete){
+            var label = schedules[index][0],
+                time = schedules[index][1];
+            
+            setters.label(label);
+            setters.progress(1000);
+            setters.remaining(sec2time(time*60));
+            countdown(() => {
+                schedules.length > index+1
+                ? runSchedule(schedules, index+1, setters, complete)
+                : complete()
+            }, time*60e3)
+            .onTick(({progress, remaining}) => {
+                setters.progress(100-progress)
+                setters.remaining(sec2time(Math.ceil(remaining/1000)))
+            },1e3).run()
+        },
+        show = function(fi){
+            append(container, [label, progress, remaining]);
+            !fi && container.removeChild(fileInput);
+        },
+        complete = function(){
+            remove(container, [label, progress, remaining]);
+            append(container, [end, rerun, newrun]);
+        },
+        setLabel = function(l){label.innerHTML = l;},
+        setProgress = function(p){progress.setAttribute('value', p)},
+        setRemaining = function(r){remaining.innerHTML = r}
 
+    end.innerHTML='TIME OVER';
+    rerun.innerHTML='↺';
+    newrun.innerHTML='new';
 
-    //
-    (function(){
-        var container = create('div', {style: styles.container}),
-            fileInput = create('input', {attrs: {type:'file', name:'file', accept:'application/json'}}),
-            // close = create('div', {style:styles.close}),
-            label = create('div', {style:styles.label}),
-            progress = create('progress', {style:styles.progress, attrs:{value: 0, max:100}}),
-            remaining = create('div', {style:styles.remaining});
-            end = create('div', {style:styles.complete});
-            newrun = create('div', {style:styles.newrun});
-            rerun = create('div', {style:styles.rerun}),
-            memRun = function (){};
-        function show(fi){
-            console.log({fi})
-            container.appendChild(label);
-            container.appendChild(progress);
-            container.appendChild(remaining);
-            if(!fi)
-                container.removeChild(fileInput);
-        }
-        function complete(){
-            container.removeChild(label);
-            container.removeChild(progress);
-            container.removeChild(remaining);
-            container.appendChild(end);
-            container.appendChild(rerun);
-            container.appendChild(newrun);
-        }
-        function setLabel(l){
-            label.innerHTML = l;
-        }
-        function setProgress(p){progress.setAttribute('value', p)}
-        function setRemaining(r){remaining.innerHTML = r}
-        // close.innerHTML='&times;';
-        end.innerHTML='TIME OVER';
-        rerun.innerHTML='↺';
-        newrun.innerHTML='new';
-        // close.addEventListener('click', function (){
-        //     target.removeChild(container);
-        // });
-        rerun.addEventListener('click', function _(){
-            container.appendChild(label);
-            container.appendChild(progress);
-            container.appendChild(remaining);
-            container.removeChild(end);
-            container.removeChild(rerun);
-            container.removeChild(newrun);
-            memRun(true);
-        })
-        newrun.addEventListener('click', function _(){
-            container.removeChild(end);
-            container.removeChild(rerun);
-            container.removeChild(newrun);
-            container.appendChild(fileInput);
-        })
-        fileInput.addEventListener('change', function (e){
-            var files = this.files;
+    rerun.addEventListener('click', function _(){
+        append(container, [label, progress, remaining]);
+        remove(container, [end, rerun, newrun]);
+        memRun(true);
+    });
 
-            if (files.length) {
-                var file = files[0],
-                    reader = new FileReader(); // Creating reader instance from FileReader() API
-                    
-                reader.addEventListener("load", function () { 
+    newrun.addEventListener('click', function _(){
+        append(container, [end, rerun, newrun]);
+        container.appendChild(fileInput);
+    });
+
+    fileInput.addEventListener('change', function (e){
+        var files = this.files,
+            file, reader;
+        if (files.length) {
+            file = files[0];
+            reader = new FileReader();
+                
+            reader.addEventListener("load", function () { 
+                try {
                     var j = JSON.parse(reader.result);
                     memRun = again => schedule(j, {
                         label: setLabel,
                         progress: setProgress,
                         remaining: setRemaining
-                    },function (){show(again);}, complete);
+                    }, function (){show(again);}, complete);
                     memRun();
-                }, false);
-                reader.readAsText(file, "UTF-8");
-                e.target.value = '';
-            }
-        });
-        container.appendChild(fileInput);
-        // container.appendChild(close);
-        target.appendChild(container);
-    })();
+                } catch(e) {
+
+                }
+            }, false);
+            reader.readAsText(file, "UTF-8");
+            e.target.value = '';
+        }
+    });
+    container.appendChild(fileInput);
+    target.appendChild(container);
 
 })();
